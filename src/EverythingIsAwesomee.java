bf22e5fde6f8371f9cb706e026df183d5133f3f0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-10 13:17:12 -0700

    Everything is Awesomee,

--- src/EverythingIsAwesomee.java
+++ src/EverythingIsAwesomee.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class EverythingIsAwesomee { public static void main(String[] args) {
  String question= JOptionPane.showInputDialog("What do you like to do?");
- JOptionPane.showMessageDialog(null, question  + "Is Awesome!");
+ JOptionPane.showMessageDialog(null, question+ "Is Awesome!");
 }
 
 }
